576f8c0a333a87ea2e3fd6af8b2556f8109d0e21
Pranav-Sharma-FC <psharma@ftstudent.org>
2026-02-19 12:09:54 -0500

    Chessboard Base Mechanics

--- Scripts/Chessboard.cs
+++ Scripts/Chessboard.cs
@@ -3,5 +3,12 @@ using System;
 
 public partial class Chessboard : TileMapLayer
 {
-	
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector2 mapCords = MapToLocal((Vector2I)GetGlobalMousePosition());
+		mapCords /= new Vector2(10000, 10000);
+		mapCords += new Vector2(1, 1);
+		mapCords = new Vector2(Mathf.Round(mapCords.X), Mathf.Round((mapCords.X)));
+		GD.Print(mapCords);
+	}
 }
