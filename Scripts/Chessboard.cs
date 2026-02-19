d324e6cb61e748fbb51db2a08a07f03ba6e95dc7
Pranav-Sharma-FC <psharma@ftstudent.org>
2026-02-19 12:37:10 -0500

    Started Selection Mechanics
    Started the Mechanics to select pieces and be able to move them onto specific squares.

--- Scripts/Chessboard.cs
+++ Scripts/Chessboard.cs
@@ -3,12 +3,27 @@ using System;
 
 public partial class Chessboard : TileMapLayer
 {
+	private Pawn _selectedPiece;
+	private bool _isSelected;
 	public override void _PhysicsProcess(double delta)
+	{
+		Vector2 mapCords = Cordinates(); 
+		GD.Print(mapCords);
+
+		if (_isSelected)
+		{
+			
+		}
+	}
+
+	private Vector2 Cordinates()
 	{
 		Vector2 mapCords = MapToLocal((Vector2I)GetGlobalMousePosition());
 		mapCords /= new Vector2(10000, 10000);
 		mapCords += new Vector2(1, 1);
 		mapCords = new Vector2(Mathf.Round(mapCords.X), Mathf.Round((mapCords.X)));
-		//GD.Print(mapCords);
+		return mapCords;
 	}
+	
+	
 }
--- Scripts/Piece.cs
+++ Scripts/Piece.cs
@@ -4,4 +4,5 @@ using System;
 public partial class Piece : CharacterBody2D
 {
 	[Export] private PackedScene pieceScene;
+	[Export] private Vector2 pieceCord;
 }
