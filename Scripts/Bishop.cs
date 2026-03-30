865ddde1b336400d0acb7a332ba6c499610ca6b7
ismith3-art <ismith3@ftstudent.org>
2026-03-30 12:21:06 -0400

    creates bishop

---
+++ Scripts/Bishop.cs
@@ -0,0 +1,29 @@
+using Godot;
+using System;
+
+public partial class Bishop : Piece
+{
+	protected override void SetPoints()
+	{
+		
+	}
+
+	public override void GivePiece()
+	{
+		
+	}
+
+	//Logic to make sure piece can move there
+	public override bool Move(Vector2I NextPosition, Vector2I CurrentPosition)
+	{
+		GD.Print("Hi");
+		bool moveFlag = true;
+		if(!(Math.Abs(CurrentPosition.X-NextPosition.X)==Math.Abs(CurrentPosition.Y-NextPosition.Y)))
+		{
+			
+			moveFlag = false;
+		}
+		GD.Print(moveFlag);
+		return moveFlag;
+	}
+}
