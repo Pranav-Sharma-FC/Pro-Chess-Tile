efc376ae499bb7c044cc2b10ec330b00132dd432
anoor-2 <anoor@ftstudent.org>
2026-03-30 12:23:05 -0400

    Code!

---
+++ Scripts/Rook.cs
@@ -0,0 +1,33 @@
+using Godot;
+using System;
+
+public partial class Rook : Piece
+{
+	protected override void SetPoints()
+	{
+		
+	}
+
+	public override void GivePiece()
+	{
+		
+	}
+	
+	//Logic to make sure piece can move there
+	public override bool Move(Vector2I NextPosition, Vector2I CurrentPosition)
+	{
+		GD.Print("Hi");
+		bool moveFlag = true;
+		if(!(NextPosition.X == (CurrentPosition.X)))
+		{
+			moveFlag = false;
+		}
+		if (!(NextPosition.Y == (CurrentPosition.Y)))
+		{
+			moveFlag = false;
+		}
+		
+		GD.Print(moveFlag);
+		return moveFlag;
+	}
+}
