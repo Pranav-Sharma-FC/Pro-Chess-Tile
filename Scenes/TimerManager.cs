009ea07807bd79369d7dfdfa64558101c22817ad
jkaur46 <jkaur46@ftstudent.org>
2026-04-02 12:36:22 -0400

    k

--- Scenes/TimerManager.cs
+++ Scenes/TimerManager.cs
@@ -5,9 +5,17 @@ public partial class TimerManager : Node
 {
 [Export] public Label WhiteLabel;
 [Export] public Label BlackLabel;
+[Export] public Label WhiteManaLabel;
+[Export] public Label BlackManaLabel;
+
 
 private float whiteTime = 600f;
 private float blackTime = 600f;
+
+private int whiteMana = 1;
+private int blackMana = 1;
+private float manaTimer = 0f;
+private float manaInterval = 30f;
 
 private bool isWhiteTurn = true;
 
@@ -16,9 +24,22 @@ public override void _Process(double delta)
 float d = (float)delta;
 
 if (isWhiteTurn)
-whiteTime -= d;
+{
+	whiteTime -= d;
+ }
+
 else
-blackTime -= d;
+{
+	blackTime -= d;
+}
+// mana timer
+manaTimer += d;
+if (manaTimer >= manaInterval)
+{
+	manaTimer = 0f;
+	whiteMana += 1;
+	blackMana += 1;
+}
 
 UpdateUI();
 }
@@ -27,6 +48,8 @@ private void UpdateUI()
 {
 WhiteLabel.Text = "White: " + FormatTime(whiteTime);
 BlackLabel.Text = "Black: " + FormatTime(blackTime);
+WhiteManaLabel.Text = "White Mana: " + whiteMana;
+BlackManaLabel.Text = "Black Mana: " + blackMana;
 }
 
 private string FormatTime(float time)
