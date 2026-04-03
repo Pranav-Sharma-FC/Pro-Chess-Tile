5ab67a48923418a41c8d3a51d61d3ea84547f57b
Pranav-Sharma-FC <psharma@ftstudent.org>
2026-04-03 14:11:33 -0400

    Movement Blocking Part ???

--- Scripts/MovementResource.cs
+++ Scripts/MovementResource.cs
@@ -3,5 +3,5 @@ using Godot;
 public partial class MovementResource : Resource
 {
 	[Export] public int xmov, ymov;
-	[Export] public Vector2I closest;
+	[Export] public Vector2I closest = new Vector2I(-1, -1);
 }
--- Scripts/Rook.cs
+++ Scripts/Rook.cs
@@ -3,6 +3,7 @@ using Godot.Collections;
 using System;
 using System.Collections.Generic;
 using System.Runtime.InteropServices.JavaScript;
+using System.Runtime.Loader;
 
 public partial class Rook : Piece
 {
@@ -20,42 +21,36 @@ public partial class Rook : Piece
 
 	public override void PieceBlocking(Vector2I CurrentPosition, Tile[,]  tiles)
 	{
+		GD.Print("u");
+		CurrentPosition -= new Vector2I(1, 1);
 		foreach (MovementResource moveResource in Movements)
 		{
-			int xstart = FindStart(moveResource.xmov, CurrentPosition.X);
-			int ystart = FindStart(moveResource.ymov, CurrentPosition.Y);
-			Vector2I newPosition = new Vector2I(xstart, ystart);
-			moveResource.closest = newPosition;
-			while(newPosition != CurrentPosition)
+			GD.Print(moveResource.xmov + " : " + moveResource.ymov);
+			moveResource.closest = new Vector2I(-1, -1);
+			Vector2I newPosition = new Vector2I(CurrentPosition.X, CurrentPosition.Y);
+			GD.Print("NewPosition: " + newPosition);
+			bool flag = true;
+			while(flag)
 			{
-				GD.Print(newPosition);
-				Tile tile = tiles[newPosition.X, newPosition.Y];
-				if (!tile.hasPieceNot());
+				if(CurrentPosition != newPosition)
 				{
-					moveResource.closest = newPosition;
+					Tile tile = tiles[newPosition.X, newPosition.Y];
+					if (!tile.hasPieceNot());
+					{
+						GD.Print(newPosition + "PositionTile");
+						moveResource.closest = newPosition;
+						flag = false;
+					}
 				}
 				newPosition.X += moveResource.xmov;
 				newPosition.Y += moveResource.ymov;
-			}
-		}
-	}
 
-	private int FindStart(int vari, int current)
-	{
-		if (vari == 0)
-			return current;
-		else if (vari == 1)
-		{
-			return 0;
-		}
-		else if (vari == -1)
-		{
-			return (8-current);
-		}
-		else
-		{
-			GD.Print("Someone screwed up");
-			return 42;
+				if (newPosition.X is >= 0 and < 8 || newPosition.Y is >= 0 and < 8)
+				{
+					flag = false;
+				}
+				
+			}
 		}
 	}
 
@@ -63,11 +58,11 @@ public partial class Rook : Piece
 	{
 		int xNext = 0;
 		int yNext = 0;
-		if (x < 0)
+		if (x > 0)
 		{
 			xNext = 1;
 		}
-		else if (x > 0)
+		else if (x < 0)
 		{
 			xNext = -1;
 		}
@@ -95,16 +90,24 @@ public partial class Rook : Piece
 			int xmoved = NextPosition.X - CurrentPosition.X;
 			Vector2I movementSlope = FindSlope(xmoved, ymoved);
 			Vector2I closestCurrent = new Vector2I(-1, -1);
+			GD.Print(movementSlope + "EEEEE");
 			foreach (MovementResource moveResource in Movements)
 			{
+				GD.Print(moveResource.closest + " : Very Current");
 				if ((moveResource.xmov == movementSlope.X) && (moveResource.ymov == movementSlope.Y))
 				{
+					GD.Print("Identified");
 					closestCurrent = moveResource.closest;
+					GD.Print(moveResource.closest);
 				}
 			}
-
-			if (((NextPosition.X-CurrentPosition.X)<=int.Abs(closestCurrent.X-CurrentPosition.X))&&
-				((NextPosition.Y-CurrentPosition.Y)<=int.Abs(closestCurrent.Y-CurrentPosition.Y)))
+			Vector2I toNext = (CurrentPosition-NextPosition).Abs();
+			GD.Print(toNext);
+			Vector2I toClosest = (CurrentPosition-closestCurrent).Abs();
+			GD.Print(toClosest);
+			if (closestCurrent == new Vector2I(-1, -1)) 
+				moveFlag = true;
+			else if (toNext <= toClosest)
 			{
 				moveFlag = true;
 			}
